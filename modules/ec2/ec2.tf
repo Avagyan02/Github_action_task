5d6e70f10f9d7bc3830458baeef97dfe413a9df8
Sam <samvelavagyan91@gmail.com>
2024-06-09 15:09:58 +0400

    add other part

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
   ami = var.ec2_ami
 
-  key_name               = var.ec2_key_name
+  # key_name               = var.ec2_key_name
   instance_type          = var.ec2_instance_type
   subnet_id              = var.subnet_id
   # vpc_security_group_ids = [aws_security_group.my_sg.id]
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,16 +1,16 @@
 variable "ec2_ami" {
   type = string
-  default = "ami-02c41ffdfd4172736"
+  default = "ami-01e444924a2233b07"
 }
 
-variable "ec2_key_name" {
-  type = string
-  default = "id_rsa"
-}
+# variable "ec2_key_name" {
+#   type = string
+#   default = "id_rsa"
+# }
 
 variable "ec2_instance_type" {
   type = string
-  default = "t2.nano"
+  default = "t2.micro"
 }
 
 variable "ec2_name" {
